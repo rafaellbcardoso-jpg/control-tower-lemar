d6a51fa061c59fa0dc223898ec7594b6c1e96e14
rafaellbcardoso-jpg <rafael.lbcardoso@gmail.com>
2026-03-13 16:43:23 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Control Tower Lemar", layout="wide")
+
+st.title("🚚 Control Tower Logística")
+
+st.write("Painel operacional de programação de motoristas")
